1748f75fb37740f3899effcb19bc710faea64063
evenoahchoi <eorkfclsrn@naver.com>
2024-02-16 04:50:18 +0900

    헤더, 푸터, 슬라이더 작성

---
+++ app/carousel.js
@@ -0,0 +1,48 @@
+'use client'
+
+import React, { useState, useEffect } from 'react';
+
+const Carousel = ({ images }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000); // 3초마다 이미지 변경
+
+    return () => clearInterval(interval);
+  }, [images.length]);
+
+  const goToIndex = (index) => {
+    setCurrentIndex(index);
+  };
+
+  return (
+    <div className="relative">
+      <div className="overflow-hidden w-1/2 mx-auto">
+        {images.map((image, index) => (
+          <div
+            key={index}
+            className={`transition-transform ease-in-out duration-700 ${index === currentIndex ? 'opacity-100' : 'opacity-0 absolute'} w-full`}
+          >
+            <img src={image.src} alt={image.alt} className="block w-full" />
+          </div>
+        ))}
+      </div>
+      {/* Indicator 버튼들 */}
+      <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 flex space-x-2">
+        {images.map((image, index) => (
+          <button
+            key={index}
+            style={{ height: '4px', width: '50px', marginBottom: '12px', backgroundColor: currentIndex === index ? 'white' : 'gray' }} // style 속성으로 직접 스타일 적용
+            onClick={() => goToIndex(index)}
+            aria-label={`Go to slide ${index + 1}`}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
+
--- app/page.js
+++ app/page.js
@@ -1,7 +1,18 @@
+import React from 'react';
+import Carousel from './carousel';
 export default function Home() {
+  // Carousel에 표시될 이미지 배열
+  const images = [
+    { src: 'https://tecdn.b-cdn.net/img/Photos/Slides/img%20(22).jpg', alt: 'Image 1' },
+    { src: 'https://tecdn.b-cdn.net/img/Photos/Slides/img%20(23).jpg', alt: 'Image 2' },
+    { src: 'https://tecdn.b-cdn.net/img/Photos/Slides/img%20(15).jpg', alt: 'Image 3' },
+  ];
+
   return (
-    <>
-      <h1>홈페이지</h1>
-    </>
+    <div>
+      <div className="m-5">
+        <Carousel images={images} />
+      </div>
+    </div>
   );
 }
