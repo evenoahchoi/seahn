f153f332107085d39efdb6d49eff9bce05ef4cc2
evenoahchoi <eorkfclsrn@naver.com>
2024-02-17 06:32:36 +0900

    오시는길 추가

--- app/about/location/page.js
+++ app/about/location/page.js
@@ -1,8 +1,104 @@
+import Image from "next/image";
+
 export default function Location() {
-    return (
-      <>
-        <h1>오시는길</h1>
-      </>
-    );
-  }
-  
+  return (
+    <div>
+      <section className="text-gray-600 body-font relative">
+        <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
+          <div className="lg:w-2/3 md:w-1/2 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+            <a
+              href="https://map.naver.com/p/entry/place/1650919378?placePath=%2Fhome&c=15.00,0,0,0,dh"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="absolute inset-0"
+            >
+              <Image
+                src="/map.png"
+                alt="지도1"
+                width={900}
+                height={900}
+                priority={true}
+              />
+            </a>
+            {/* 박스 전체를 flex container로 만들고, 주소를 포함하는 div의 너비를 조정합니다. */}
+            <div className="bg-white relative flex flex-wrap py-1 rounded shadow-md w-3/4">
+              {/* 주소 정보가 더 많은 공간을 차지하도록 flex-grow와 flex-basis를 사용합니다. */}
+              <div className="flex-grow flex-basis-0 px-6">
+                <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">
+                  ADDRESS
+                </h2>
+                {/* whitespace-nowrap 클래스를 추가하여 주소가 한 줄에 나오도록 합니다. */}
+                <p className="mt-1 text-base whitespace-nowrap">
+                  경기 군포시 엘에스로 13 신일IT유토 1301호
+                </p>
+              </div>
+              <div className="flex-grow flex-basis-0 px-6">
+                <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">
+                  EMAIL
+                </h2>
+                <a className="mt-1 text-base">example@email.com</a>
+                <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">
+                  PHONE
+                </h2>
+                <p className="leading-relaxed text-base">123-456-7890</p>
+              </div>
+            </div>
+          </div>
+          <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+            <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
+              Feedback
+            </h2>
+            <p className="leading-relaxed mb-5 text-gray-600">
+              Post-ironic portland shabby chic echo park, banjo fashion axe
+            </p>
+            <div className="relative mb-4">
+              <label htmlFor="name" className="leading-7 text-sm text-gray-600">
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <div className="relative mb-4">
+              <label
+                htmlFor="email"
+                className="leading-7 text-sm text-gray-600"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <div className="relative mb-4">
+              <label
+                htmlFor="message"
+                className="leading-7 text-sm text-gray-600"
+              >
+                Message
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+              ></textarea>
+            </div>
+            <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+              Button
+            </button>
+            <p className="text-xs text-gray-500 mt-3">
+              Chicharrones blog helvetica normcore iceland tousled brook viral
+              artisan.
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
