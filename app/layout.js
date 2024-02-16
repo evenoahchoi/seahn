daca5b66b731f7ef260fe0dbf09f64df6346fb3e
evenoahchoi <eorkfclsrn@naver.com>
2024-02-17 08:30:57 +0900

    레이아웃 Link태그 수정

--- app/layout.js
+++ app/layout.js
@@ -16,113 +16,92 @@ export default function RootLayout({ children }) {
           {" "}
           {/* 하단 선을 추가 */}
           <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-            <Link legacyBehavior href="/">
-              <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-                <Image
-                  src="/seahn.png"
-                  alt="세안전자 로고"
-                  width={100}
-                  height={100}
-                  priority={true}
-                />
-                <span className="ml-3 text-xl font-bold">세안전자</span>
-              </a>
+            <Link
+              className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+              href="/"
+            >
+              <Image
+                src="/seahn.png"
+                alt="세안전자 로고"
+                width={100}
+                height={100}
+                priority={true}
+              />
+              <span className="ml-3 text-xl font-bold">세안전자</span>
             </Link>
             <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
               {/* 회사소개 드롭다운 메뉴 */}
               <div className="relative group">
-                <Link legacyBehavior href="/about">
-                  <a className="mr-5 hover:text-gray-900 inline-flex items-center">
-                    회사소개
-                  </a>
+                <Link
+                  className="mr-5 hover:text-gray-900 inline-flex items-center"
+                  href="/about/ceo"
+                >
+                  회사소개
                 </Link>
                 <div className="absolute hidden group-hover:block bg-white text-base z-50 min-w-max dropdown-menu">
-                  <a
-                    href="/about/ceo"
-                    className="dropdown-link"
-                  >
+                  <Link href="/about/ceo" className="dropdown-link">
                     CEO 인사말
-                  </a>
-                  <a
-                    href="/about/comhistory"
-                    className="dropdown-link"
-                  >
+                  </Link>
+                  <Link href="/about/comhistory" className="dropdown-link">
                     회사연혁
-                  </a>
-                  <a
-                    href="/about/policy"
-                    className="dropdown-link"
-                  >
+                  </Link>
+                  <Link href="/about/policy" className="dropdown-link">
                     경영방침
-                  </a>
-                  <a
-                    href="/about/certy"
-                    className="dropdown-link"
-                  >
+                  </Link>
+                  <Link href="/about/certy" className="dropdown-link">
                     인증서
-                  </a>
-                  <a
-                    href="/about/location"
-                    className="dropdown-link"
-                  >
+                  </Link>
+                  <Link href="/about/location" className="dropdown-link">
                     오시는길
-                  </a>
+                  </Link>
                 </div>
               </div>
               <div className="relative group">
-                <Link legacyBehavior href="/products">
-                  <a className="mr-5 hover:text-gray-900 inline-flex items-center">
-                    제품정보
-                  </a>
+                <Link
+                  className="mr-5 hover:text-gray-900 inline-flex items-center"
+                  href="/products/transformer"
+                >
+                  제품정보
                 </Link>
                 <div className="absolute hidden group-hover:block bg-white text-base z-50 min-w-max dropdown-menu">
-                  <a
-                    href="/products/transformer"
-                    className="dropdown-link"
-                  >
+                  <Link href="/products/transformer" className="dropdown-link">
                     TRANSFORMER
-                  </a>
-                  <a
-                    href="/products/inductor"
-                    className="dropdown-link"                  >
+                  </Link>
+                  <Link href="/products/inductor" className="dropdown-link">
                     INDUCTOR
-                  </a>
-                  <a
-                    href="/products/linefilter"
-                    className="dropdown-link"
-                  >
+                  </Link>
+                  <Link href="/products/linefilter" className="dropdown-link">
                     LINE FILTER
-                  </a>
+                  </Link>
                 </div>
               </div>
               <div className="relative group">
-                <Link legacyBehavior href="/technology">
-                  <a className="mr-5 hover:text-gray-900 inline-flex items-center">
-                    기술정보
-                  </a>
+                <Link
+                  className="mr-5 hover:text-gray-900 inline-flex items-center"
+                  href="/technology/inductorinfo"
+                >
+                  기술정보
                 </Link>
                 <div className="absolute hidden group-hover:block bg-white text-base z-50 min-w-max dropdown-menu">
-                  <a
+                  <Link
                     href="/technology/inductorinfo"
                     className="dropdown-link"
                   >
                     INDUCTOR정보
-                  </a>
+                  </Link>
                 </div>
               </div>
               <div className="relative group">
-                <Link legacyBehavior href="/support">
-                  <a className="mr-5 hover:text-gray-900 inline-flex items-center">
-                    고객지원
-                  </a>
+                <Link
+                  className="mr-5 hover:text-gray-900 inline-flex items-center"
+                  href="/support"
+                >
+                  고객지원
                 </Link>
                 <div className="absolute hidden group-hover:block bg-white text-base z-50 min-w-max dropdown-menu">
-                  <a
-                    href="/support/samp"
-                    className="dropdown-link"
-                  >
+                  <Link href="/support/samp" className="dropdown-link">
                     샘플신청
-                  </a>
+                  </Link>
                 </div>
               </div>
             </nav>
@@ -132,17 +111,18 @@ export default function RootLayout({ children }) {
         <footer class="bg-white text-gray-600 body-font border-t border-gray-300">
           <div class="container px-5 py-5 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
             <div class="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
-              <Link legacyBehavior href="/">
-                <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-                  <Image
-                    src="/seahn.png"
-                    alt="세안전자 로고"
-                    width={50}
-                    height={50}
-                    priority={true}
-                  />
-                  <span class="ml-3 text-xl font-bold">세안전자</span>
-                </a>
+              <Link
+                class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900"
+                href="/"
+              >
+                <Image
+                  src="/seahn.png"
+                  alt="세안전자 로고"
+                  width={50}
+                  height={50}
+                  priority={true}
+                />
+                <span class="ml-3 text-xl font-bold">세안전자</span>
               </Link>
               <p class="mt-2 text-sm text-gray-500">
                 세상을 더욱 살기 좋게 만듭니다
@@ -155,19 +135,44 @@ export default function RootLayout({ children }) {
                 </h2>
                 <nav class="list-none mb-10">
                   <li>
-                    <a href="/about/ceo" class="text-gray-600 hover:text-gray-800">CEO 인사말</a>
+                    <Link
+                      href="/about/ceo"
+                      class="text-gray-600 hover:text-gray-800"
+                    >
+                      CEO 인사말
+                    </Link>
                   </li>
                   <li>
-                    <a href="/about/comhistory" class="text-gray-600 hover:text-gray-800">회사연혁</a>
+                    <Link
+                      href="/about/comhistory"
+                      class="text-gray-600 hover:text-gray-800"
+                    >
+                      회사연혁
+                    </Link>
                   </li>
                   <li>
-                    <a href="/about/policy" class="text-gray-600 hover:text-gray-800">경영방침</a>
+                    <Link
+                      href="/about/policy"
+                      class="text-gray-600 hover:text-gray-800"
+                    >
+                      경영방침
+                    </Link>
                   </li>
                   <li>
-                    <a href="/about/certy" class="text-gray-600 hover:text-gray-800">인증서</a>
+                    <Link
+                      href="/about/certy"
+                      class="text-gray-600 hover:text-gray-800"
+                    >
+                      인증서
+                    </Link>
                   </li>
                   <li>
-                    <a href="/about/location" class="text-gray-600 hover:text-gray-800">오시는길</a>
+                    <Link
+                      href="/about/location"
+                      class="text-gray-600 hover:text-gray-800"
+                    >
+                      오시는길
+                    </Link>
                   </li>
                 </nav>
               </div>
@@ -177,13 +182,28 @@ export default function RootLayout({ children }) {
                 </h2>
                 <nav class="list-none mb-10">
                   <li>
-                    <a href="/products/transformer" class="text-gray-600 hover:text-gray-800">TRANSFORMER</a>
+                    <Link
+                      href="/products/transformer"
+                      class="text-gray-600 hover:text-gray-800"
+                    >
+                      TRANSFORMER
+                    </Link>
                   </li>
                   <li>
-                    <a href="/products/inductor" class="text-gray-600 hover:text-gray-800">INDUCTOR</a>
+                    <Link
+                      href="/products/inductor"
+                      class="text-gray-600 hover:text-gray-800"
+                    >
+                      INDUCTOR
+                    </Link>
                   </li>
                   <li>
-                    <a href="/products/linefilter" class="text-gray-600 hover:text-gray-800">LINE FILTER</a>
+                    <Link
+                      href="/products/linefilter"
+                      class="text-gray-600 hover:text-gray-800"
+                    >
+                      LINE FILTER
+                    </Link>
                   </li>
                 </nav>
               </div>
@@ -193,9 +213,12 @@ export default function RootLayout({ children }) {
                 </h2>
                 <nav class="list-none mb-10">
                   <li>
-                    <a href="/technology/inductorinfo" class="text-gray-600 hover:text-gray-800">
+                    <Link
+                      href="/technology/inductorinfo"
+                      class="text-gray-600 hover:text-gray-800"
+                    >
                       INDUCTOR정보
-                    </a>
+                    </Link>
                   </li>
                 </nav>
               </div>
@@ -205,7 +228,12 @@ export default function RootLayout({ children }) {
                 </h2>
                 <nav class="list-none mb-10">
                   <li>
-                    <a href="/support/samp" class="text-gray-600 hover:text-gray-800">샘플신청</a>
+                    <a
+                      href="/support/samp"
+                      class="text-gray-600 hover:text-gray-800"
+                    >
+                      샘플신청
+                    </a>
                   </li>
                 </nav>
               </div>
